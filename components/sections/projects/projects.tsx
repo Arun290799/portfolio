92685c9927bbca6de43f87a27b91bcbf9277adc3
Arun <polojuarunkumar2@gmail.com>
2026-04-05 18:08:15 +0530

    minor bug fix

--- components/sections/projects/projects.tsx
+++ components/sections/projects/projects.tsx
@@ -64,7 +64,7 @@ export default function Projects() {
 												<a
 													href={project.links.github}
 													target="_blank"
-													className="inline-flex items-center justify-center w-10 h-10 rounded-lg text-primary border border-white/20 hover:bg-white hover:text-black transition-all duration-50"
+													className="inline-flex items-center justify-center w-10 h-10 rounded-lg text-primary border border-white/20 hover:bg-gray-800 hover:text-gray-600 transition-all duration-50"
 												>
 													<svg viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
 														<path d="M12 .5C5.73.5.5 5.73.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.56v-2.02c-3.2.7-3.87-1.54-3.87-1.54-.53-1.35-1.3-1.7-1.3-1.7-1.06-.73.08-.72.08-.72 1.17.08 1.78 1.2 1.78 1.2 1.04 1.78 2.74 1.27 3.4.97.1-.76.4-1.27.72-1.56-2.55-.29-5.23-1.28-5.23-5.68 0-1.25.45-2.27 1.2-3.07-.12-.3-.52-1.52.11-3.17 0 0 .98-.31 3.2 1.17a11.1 11.1 0 0 1 5.82 0c2.22-1.48 3.2-1.17 3.2-1.17.63 1.65.23 2.87.11 3.17.75.8 1.2 1.82 1.2 3.07 0 4.41-2.69 5.38-5.25 5.67.41.36.77 1.08.77 2.17v3.22c0 .31.21.68.8.56A10.51 10.51 0 0 0 23.5 12C23.5 5.73 18.27.5 12 .5z" />
